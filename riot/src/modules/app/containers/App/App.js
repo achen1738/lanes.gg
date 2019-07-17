28e3ec792056f575e425425c9450b91a34fd340b
achen1738 <ac99@rice.edu>
2019-07-16 22:43:24 -0500

    Reset some weird changes i made

--- riot/src/modules/app/containers/App/App.js
+++ riot/src/modules/app/containers/App/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return <div className="App">SAD</div>;
   }
 }
 
